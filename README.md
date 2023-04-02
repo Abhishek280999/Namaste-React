68e62efc263e6a1aa3f84218b0c6465144bce118
Abhishek Darwekar <darwekar155@gmail.com>
2023-04-02 14:42:52 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Namaste React
+
+
+# Parcel 
+
+- DEv Build
+- Local server
+- HMR - Hot module replacement  ( live server)
+- File watching Algorithms
+- Caching - Faster Builds 
+- Image Optimization
+- Minification
+- bundling
+- Compress
+- code splitting
+- Consistent Hashing
+- Differential Bundling - to support older brower versions
+- Error handling
+- Diagnostic.
+- Https 
+- Tree Shaking - Remove Unused Code 
+ 
--- app.js
+++ app.js
@@ -1,26 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
- [
-    React.createElement("div",{ id: "child" },
-    [
-        React.createElement("h1", {}, "I am an h1 tag"), 
-        React.createElement("h2", {}, "I am an h2 tag")   
-    ]),
-    React.createElement("div",{ id: "child2 " },
-    [
-        React.createElement("h1", {}, "I am an h1 tag"), 
-        React.createElement("h2", {}, "I am an h2 tag")    
-    ])
- ]
-);
+//React elements
+//React.createElement => Object => HTMLElement(render)
 
+const heading = React.createElement(
+    "h1",
+    { id: 'heading'},
+    "Namaste React"
+    );
+    console.log(heading);
 
-console.log(parent); 
+    //JSX - i not HTML in JS
 
+const jsxheading = <h1>Namaste React Using JSX</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
 
 
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-
-root.render(parent);
