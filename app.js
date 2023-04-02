39fae11b06762f7790f1e937f87556fc43daff9d
Abhishek Darwekar <darwekar155@gmail.com>
2023-04-02 20:35:55 +0530

    React fragment

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 
 //React elements
 //React.createElement => Object => HTMLElement(render)
-
-const Title =()=>(
+const elements = <span>React Element</span>
+const Title = () =>(
     <h1 id="heading" className="root">
+   
   Namaste React Using JSX
-  </h1>)
+  
+  </h1>
+  );
 
 //React components - 2 types 
  // Functional component  -- NEW way
@@ -17,15 +20,26 @@ const Title =()=>(
 
 //  const HeadingComponent = () => <h1>Namaste React Using Functional component</h1>
 
-            //OR 
+
+//OR
+   // Component composition
+
+   //REact Fragment - behavior like an emety
  const HeadingComponent = () =>(
+   <React.Fragment>
+    /* <>   */
     <div id="container">
-    <Title/>    
+    {Title()}
+    <Title></Title>    
         <h1 id="heading">Namaste React Using Functional component</h1>
     </div>
- )
+    <div id="container">   
+        <h1 id="heading">Namaste React Using Functional component</h1>
+    </div>
+     /* </>   */
+    </React.Fragment>
+ )   
 
-    
 
  // <HeadingComponent/>
 
